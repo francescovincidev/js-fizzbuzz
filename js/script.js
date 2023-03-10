f0ba4580d6b6ba3bb8ab8a4e81a822df85905262
francescovincidev <francescovinci2000@gmail.com>
2023-03-10 13:14:44 +0100

    Without Bonus

---
+++ js/script.js
@@ -0,0 +1,26 @@
+let toPrint;
+
+// Ciclo
+for (let i = 1; i <= 100; i++) {
+
+    // Diamo alla vraiabile il valore di i
+    toPrint = i;
+
+    // SE è divisibile per 3 e 5 gli diamo il valore di "FizzBuzz"
+    if(i % 3 === 0  && i % 5 === 0){
+        toPrint = "FizzBuzz";
+
+    // SE è divisibile per 3 gli diamo il valore di "Fizz"
+
+    } else if(i % 3 === 0){
+        toPrint = "Fizz";
+
+    // SE è divisibile per 5 gli diamo il valore di "Buzz"
+
+    }else if(i % 5 === 0){
+        toPrint = "Buzz";
+    }
+
+    // Output
+    console.log(toPrint);
+}
